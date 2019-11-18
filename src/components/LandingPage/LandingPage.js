228af131d3aabb55988d73977b4d4e4b86429431
Jess Dam <Jess Dam>
2019-11-18 23:05:37 +0000

    stylign for landing page, added react bootstrap dropdown and logo in own directory

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import { Dropdown, DropdownButton } from 'react-bootstrap'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './CSS/LandingPage.css'
 
 function LandingPage(){
     return(
-        <div>
-        <h1>Welcome to spaceNET</h1>
-        <h4>What would you like to explore?</h4>
+        <div className='context-box'>
+            <img src='../../../logo/spacenet-logo.png' alt='logo' width='300' height='300'/>
+            <h1 className='title'>Welcome to spaceNET</h1>
+            <div className='dropdown-box'>
+                <h4 className='sub-text'>What would you like to explore?</h4>
+                <DropdownButton id="dropdown-navigator" title="explore..." className='dropdown' variant='dark'>
+                    <Dropdown.Item href="#/action-1">Asteroids</Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item href="#/action-2">Urgent Space News</Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item href="#/action-2">Mars</Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item href="#/action-3">Astronomy Photo of the Day (APOD)</Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item href="#/action-2">Earth Observatory Natural Event Tracker (EONET)</Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item href="#/action-2">Take me to the home page</Dropdown.Item>
+                </DropdownButton>
+
+                <button
+                    className='go-button'
+                    onClick={() => {}}
+                >Go</button>
+
+
+            </div>
 
         </div>
     )
