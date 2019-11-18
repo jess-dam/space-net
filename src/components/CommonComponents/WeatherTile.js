3084701ff9f9ed1e5af85bd84955f23421b0a0a3
Dawid <dawid.budaszewski1@gmail.com>
2019-11-18 15:06:02 +0000

    Mars tiles are done

---
+++ src/components/CommonComponents/WeatherTile.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import styles from './CSS/WeatherTile.module.css'
+
+function WeatherTile ({ solString, data }) {
+  const sol = Number(solString)
+  console.log(data)
+  return (
+    <div className={styles['border']}>
+      <h1>Some date I guess</h1>
+      <div>
+        <svg width="163" height="127" viewBox="0 0 163 127" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="131.5" cy="32.5" r="31.5" fill="#F2EA23"/>
+        <ellipse cx="64.5" cy="63.5" rx="64.5" ry="63.5" fill="#A85D27"/>
+        </svg>
+      </div>
+      <h2>Sol: {sol}</h2>
+      <div className={styles['weather-details']}>
+        <div>
+          <h4>Temp:</h4>
+          <p>Avg: {data[sol].AT.av}</p>
+          <p>Min: {data[sol].AT.mn}</p>
+          <p>Max: {data[sol].AT.mx}</p>
+        </div>
+        <div>
+          <h4>Wind:</h4>
+          <p>Av: {data[sol].HWS.av}</p>
+          <p>Min: {data[sol].HWS.mn}</p>
+          <p>Max: {data[sol].HWS.mx}</p>
+        </div>
+        <div>
+          <h4>Wind:</h4>
+          <p>Av: {data[sol].PRE.av}</p>
+          <p>Min: {data[sol].PRE.mn}</p>
+          <p>Max: {data[sol].PRE.mx}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default WeatherTile
--- src/components/Content/Mars/Mars.js
+++ src/components/Content/Mars/Mars.js
@@ -1,21 +1,32 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import WeatherTile from './../../CommonComponents/WeatherTile'
 
 function Mars () {
-  const [data, setData] = useState(null)
-  useEffect(() => {
+  const [data, setData] = useState()
+  const [helper, setHelper] = useState()
+    useEffect(() => {
     const fetchData = async () => {
       const results = await axios(
         'https://api.nasa.gov/insight_weather/?api_key=Enoih2fwvokMm0hHR3AwXnV4vw1I3tamZ6GBM5O4&feedtype=json&ver=1.0'
       )
       setData(results.data)
-      console.log(results)
+      console.log(results.data)
     }
     fetchData()
   }, [])
+
   return (
     <div>
-      WELCOME TO MARS
+      {
+        data && data.sol_keys.map(sol =>
+          <WeatherTile
+            key={sol}
+            solString={sol}
+            data={data}
+          />
+          )
+      }
     </div>
   )
 }
