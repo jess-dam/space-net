ce17a51b3d2def09f3b415b3f7ea757273c5ff18
Dawid <dawid.budaszewski1@gmail.com>
2019-11-19 16:42:37 +0000

    about to filter out from api

--- src/components/Content/Eonet/Earthquackes/Earthquackes.js
+++ src/components/Content/Eonet/Earthquackes/Earthquackes.js
@@ -1,35 +1,53 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+import ReactMapboxGl, { Layer, Feature, Marker } from 'react-mapbox-gl';
 import styles from './../../CSS/Eonet.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFire } from '@fortawesome/free-solid-svg-icons'
+import { faFire, faMountain } from '@fortawesome/free-solid-svg-icons'
+import Volcanoes from '../Volcanoes/Volcanoes';
 
 function Earthquackes () {
   const [data, setData] = useState(null)
-  const [numberOfEvents, setNumberOfEvents] = useState(null)
+  const [listOfEvents, setListOfEvents] = useState(null)
 
   useEffect(() => {
     const fetchData = async () => {
       const results = await axios(
         'https://eonet.sci.gsfc.nasa.gov/api/v2.1/events'
       )
+      //apply filter here
+      const dodo = results.data.events.filter(event => typeof(event.geometries[0].coordinates[0] === 'number'))
       setData(results.data)
       console.log(results)
-      const tempArr = []
+      const tempArrWildfire = []
+      const tempArrVolcanoes = []
+      const temArrAllEvents = []
       for ( let i = 0; i < results.data.events.length; i ++) {
-        tempArr.push(i)
+        temArrAllEvents.push(i)
+        if (results.data.events[i].categories[0].title === 'Wildfires') {
+          tempArrWildfire.push(i)
+        } else if (results.data.events[i].categories[0].title === 'Volcanoes') {
+          tempArrVolcanoes.push(i)
+        }
       }
-      setNumberOfEvents(tempArr)
+      setListOfEvents({
+        allEvents: [...temArrAllEvents],
+        wildfires: [...tempArrWildfire],
+        volcanoes: [...tempArrVolcanoes]
+      })
     }
     fetchData()
-
   }, [])
-
+  
   const Map = ReactMapboxGl({
     accessToken:
       'pk.eyJ1Ijoic2Vwc2FyIiwiYSI6ImNrMzV1cDBlaTBtZTMzY3BlMGtxZHgxbDgifQ.BAO2QYMsaX-CmTanG19_GQ'
   });
+
+  listOfEvents && console.log(listOfEvents.allEvents)
+  data && data.events.forEach( event => {
+    console.log(typeof(event.geometries[0].coordinates[0]))
+  })
 
   return (
     <div>
@@ -41,16 +59,27 @@ function Earthquackes () {
           width: '100vw'
         }}
       > 
-        <Layer type="symbol" id="marker" layout={{ 'icon-image': 'fire-station-15' }}>
-          {
-            data && numberOfEvents && numberOfEvents.map(event =>
-              <Feature 
-                key={event}
-                coordinates={[data.events[event].geometries['0'].coordinates[0], data.events[event].geometries['0'].coordinates[1]]}
-              />
-            )
-          }
-        </Layer>   
+        {
+          data && listOfEvents && data.events.map(event => 
+            <Marker
+              key={event}
+              coordinates={[event.geometries[0].coordinates[0], event.geometries[0].coordinates[1]]}
+            >
+              {/* { (listOfEvents.wildfires.indexOf !== -1) ? (<FontAwesomeIcon icon={faFire} />) : (<FontAwesomeIcon icon={faMountain} />)} */}
+              <FontAwesomeIcon icon={faFire} />
+            </Marker>  
+          )
+        }
+        {/* {
+          data && listOfEvents && listOfEvents.volcanoes.map(event => 
+            <Marker
+              key={event}
+              coordinates={[data.events[event].geometries['0'].coordinates[0], data.events[event].geometries['0'].coordinates[1]]}
+            >
+              <FontAwesomeIcon icon={faMountain} />
+            </Marker>  
+          )
+        } */}
       </Map>
       </div>
     </div>
@@ -58,3 +87,21 @@ function Earthquackes () {
 }
 
 export default Earthquackes
+{/* <Layer type="symbol" id="marker" layout={{ 'icon-image': 'volcano-15' }}>
+  {
+    data && listOfEvents && listOfEvents.wildfires.map(event =>
+      <Feature 
+        key={event}
+        coordinates={[data.events[event].geometries['0'].coordinates[0], data.events[event].geometries['0'].coordinates[1]]}
+      />
+    )
+  }
+  {
+    data && listOfEvents && listOfEvents.volcanoes.map(event =>
+      <Feature 
+        key={event}
+        coordinates={[data.events[event].geometries['0'].coordinates[0], data.events[event].geometries['0'].coordinates[1]]}
+      />
+    )
+  }
+</Layer> */}
