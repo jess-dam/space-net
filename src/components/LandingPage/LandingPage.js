b6f0a890adcba46627719ea5373ae9b5758d5b1a
Jess Dam <Jess Dam>
2019-11-19 10:22:40 +0000

    more efficient code for drop down with map

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -2,35 +2,40 @@ import React from 'react'
 import { Dropdown, DropdownButton } from 'react-bootstrap'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './CSS/LandingPage.css'
+import styles from './CSS/LandingPage.module.css'
+
 
 function LandingPage(){
+    const [exploreChoice, setExploreChoice] = React.useState('explore...')
+    const exploringOptions = ['Asteroids', 'Urgent Space News', 'Mars', 'Astronomy Photo of the Day (APOD)', 'Earth Observatory Natural Event Tracker (EONET)', 'Take me to the home page']
     return(
-        <div className='content-box background-box'>
-            <div className='logo'></div>
+        <div className={styles['content-box']}>
+            <div className={styles['logo']}></div>
             {/* <img src='../../../logo/spacenet-logo.png' alt='logo' width='150' height='150'/> */}
-            <h1 className='title'>Welcome to spaceNET</h1>
+            <h1 className={styles['title']}>Welcome to spaceNET</h1>
             <br></br>
             <br></br>
-            <div className='dropdown-box'>
-                <h4 className='sub-text'>What would you like to explore?</h4>
-                <DropdownButton id="dropdown-navigator" title="explore..." className='dropdown' variant='dark'>
-                    <Dropdown.Item href="#/action-1">Asteroids</Dropdown.Item>
-                    <Dropdown.Divider />
-                    <Dropdown.Item href="#/action-2">Urgent Space News</Dropdown.Item>
-                    <Dropdown.Divider />
-                    <Dropdown.Item href="#/action-2">Mars</Dropdown.Item>
-                    <Dropdown.Divider />
-                    <Dropdown.Item href="#/action-3">Astronomy Photo of the Day (APOD)</Dropdown.Item>
-                    <Dropdown.Divider />
-                    <Dropdown.Item href="#/action-2">Earth Observatory Natural Event Tracker (EONET)</Dropdown.Item>
-                    <Dropdown.Divider />
-                    <Dropdown.Item href="#/action-2">Take me to the home page</Dropdown.Item>
+            <div className={styles['dropdown-box']}>
+                <h4 className={styles['sub-text']}>What would you like to explore?</h4>
+                <DropdownButton id="dropdown-navigator" title={exploreChoice} className={styles['dropdown']} variant='dark'>
+                    {
+                        exploringOptions.map(option => {
+                            return(
+                                <>
+                                <Dropdown.Item href={`#/${option}`}>{option}</Dropdown.Item>
+                                </>
+
+                            )
+                        })
+                    }
+
                 </DropdownButton>
 
                 <button
-                    className='go-button'
-                    onClick={() => {}}
+                    className={styles['go-button']}
+                    onClick={() => {
+
+                    }}
                 >Go</button>
 
 
