fc8c54eb22d8f8f516db611553db6b7ef14040b2
Fojilun Hussain <fojilun.hussain@investec.co.uk>
2019-11-19 16:11:05 +0000

    started removing unnecessary info

--- src/components/CommonComponents/UrgentInformationBox.js
+++ src/components/CommonComponents/UrgentInformationBox.js
@@ -6,9 +6,9 @@ function UrgentInformationBox ({ notificationsResults, messageType, timeOccurred
 
         <>
 
-            <h2>Message type: {messageType}</h2>
-            <h2>Time occurred: {timeOccurred}</h2>
-            <h2>Information: {information}</h2><br  />
+            <p>Message type: {messageType}</p>
+            <p>Time occurred: {timeOccurred}</p>
+            <p>Information: {information}</p><br  />
 
         </>
 
--- src/components/Content/Urgent/Urgent.js
+++ src/components/Content/Urgent/Urgent.js
@@ -8,8 +8,6 @@ function Urgent () {
   const [solarFlare, setSolarFlare] = useState()
   const [geomagneticStorm, setGeomagneticStorm] = useState()
   const [notificationsResults, setNotificationsResults] = useState()
-  const [startIndex, setStartIndex] = useState()
-  const [newMessageBody, setNewMessageBody] = useState()
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,47 +27,15 @@ function Urgent () {
         'https://api.nasa.gov/DONKI/notifications?startDate=2019-11-10&endDate=2019-11-17&type=all&api_key=Enoih2fwvokMm0hHR3AwXnV4vw1I3tamZ6GBM5O4'
       )
       setNotificationsResults(notificationsResults.data)
-      // console.log(notificationsResults)
-      // console.log(notificationsResults.data)
-      // console.log(notificationsResults.data[0])
-      // console.log(notificationsResults.data[0].messageType)
-      // console.log(typeof notificationsResults.data[0].messageType)
-      // console.log(notificationsResults.data[0].messageIssueTime)
-      // console.log((notificationsResults.data[0].messageBody).split("Summary"))
-      // console.log(notificationsResults.data[0].messageID)
+
     }
     fetchData()
   }, [])
-
-  console.log(notificationsResults)
-  notificationsResults && notificationsResults[0] && notificationsResults[0].messageType && (
-    console.log("MESSAGETYPE"+notificationsResults[0].messageType + "INFORMATION" + notificationsResults[0].messageBody
-    + "SUMMARY INDEX" + notificationsResults[0].messageBody.indexOf("## Summary:") + "EVENTS INDEX" + 
-    notificationsResults[0].messageBody.indexOf("##Events")
-    )
-  )
 
   return (
     <div>
       <h1>Notifications</h1>
       {
-        // notificationsResults && notificationsResults[0] && notificationsResults[0].messageID &&
-        // notificationsResults[0].messageType && notificationsResults.key(
-        //   notificationsResults.messageID
-        // ).map( notification => 
-        //   <div>
-        //     <h1>Notifications</h1>
-        //     <UrgentInformationBox
-        //       key = {notificationsResults[0].messageID}
-        //       messageType = {notificationsResults[0].messageType}
-        //       timeOccurred = {notificationsResults[0].messageIssueTime}
-        //       information = {((notificationsResults[0].messageBody).slice(
-        //         (notificationsResults[0].messageBody.indexOf("## Summary:") + 11),
-        //         notificationsResults[0].messageBody.indexOf("##Events")
-        //       ))}
-        //     />
-        //   </div>
-        // )
         notificationsResults && notificationsResults.map(notificationResult => (
           <>
             <UrgentInformationBox
@@ -78,7 +44,8 @@ function Urgent () {
               information = {notificationResult.messageBody}
             />
           </>
-        ))}
+        ))
+      }
     </div>
   )
 }
