6d836cb7cf98c4771cc05f2de35ea0b5cf6525bf
Jess Dam <Jess Dam>
2019-11-19 10:42:31 +0000

    drop down now directs you appropriate page when use clicks go

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -4,10 +4,14 @@ import { Dropdown, DropdownButton } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './CSS/LandingPage.module.css'
 
+import { useHistory } from 'react-router-dom'
+
 
 function LandingPage(){
     const [exploreChoice, setExploreChoice] = React.useState('explore...')
     const exploringOptions = ['Asteroids', 'Urgent Space News', 'Mars', 'Astronomy Photo of the Day (APOD)', 'Earth Observatory Natural Event Tracker (EONET)', 'Take me to the home page']
+    const history = useHistory()
+
     return(
         <div className={styles['content-box']}>
             <div className={styles['logo']}></div>
@@ -17,16 +21,15 @@ function LandingPage(){
             <br></br>
             <div className={styles['dropdown-box']}>
                 <h4 className={styles['sub-text']}>What would you like to explore?</h4>
+
                 <DropdownButton id="dropdown-navigator" title={exploreChoice} className={styles['dropdown']} variant='dark'>
                     {
-                        exploringOptions.map(option => {
-                            return(
-                                <>
-                                <Dropdown.Item href={`#/${option}`}>{option}</Dropdown.Item>
-                                </>
+                        exploringOptions.map(option =>
+                            <>
+                            <Dropdown.Item href={`#/${option}`} onClick={() => {setExploreChoice(option)}}>{option}</Dropdown.Item>
+                            </>
 
-                            )
-                        })
+                        )
                     }
 
                 </DropdownButton>
@@ -34,7 +37,33 @@ function LandingPage(){
                 <button
                     className={styles['go-button']}
                     onClick={() => {
+                        switch(exploreChoice){
+                            case 'Asteroids':
+                                history.push(`/asteroids`)
+                                break;
+                            case (exploringOptions[1]):
+                                history.push(`/urgent`)
+                                break;
 
+                            case (exploringOptions[2]):
+                                history.push(`/mars`)
+                                break;
+
+                            case (exploringOptions[3]):
+                                history.push(`/apod`)
+                                break;
+
+                            case (exploringOptions[4]):
+                                history.push(`/eonet`)
+                                break;
+
+
+                            default:
+                                console.log(exploreChoice)
+                                console.log(exploringOptions[0])
+                                history.push(`/homePage`)
+                        }
+                        // history.push(`/${option}`)
                     }}
                 >Go</button>
 
